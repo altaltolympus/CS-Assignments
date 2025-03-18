bac06c0ef8016df97e42658eb7107af13f887f19
altaltolympus <luke.ofoghlu@colaistechilliain.com>
2025-03-18 09:26:27 +0000

    we did good

--- cereal.py
+++ cereal.py
@@ -1,8 +1,8 @@
 import serial as cereal
-s =  cereal.Serial()
+s = cereal.Serial()
 s.baudrate = 115200
 s.port = "COM3"
 s.open()
 while True:
-    compass = (str(s.readline())[2:][:-31])
+    compass = str(s.readline())
     print(compass)
