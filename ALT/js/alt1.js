4bc3537ce1f5819066ebae8cca9980716e4fdc44
alternateolympus <mountlukeolympus@gmail.com>
2025-05-23 18:13:48 +0100

    :hover my saviour

---
+++ ALT/js/alt1.js
@@ -0,0 +1,109 @@
+const barbarian = {
+    shown:false,
+    description:"Barbarians are mighty warriors who are powered by primal forces of the multiverse that manifest as a Rage. More than a mere emotion—and not limited to anger—this Rage is an incarnation of a predator’s ferocity, a storm’s fury, and a sea’s turmoil."
+}
+const bard = {
+    shown:false,
+    description:"Invoking magic through music, dance, and verse, Bards are expert at inspiring others, soothing hurts, disheartening foes, and creating illusions. Bards believe the multiverse was spoken into existence and that remnants of its Words of Creation still resound and glimmer on every plane of existence. Bardic magic attempts to harness those words, which transcend any language."
+}
+const cleric = {
+    shown:false,
+    description:"Clerics draw power from the realms of the gods and harness it to work miracles. Blessed by a deity, a pantheon, or another immortal entity, a Cleric can reach out to the divine magic of the Outer Planes—where gods dwell—and channel it to bolster people and battle foes."
+}
+const druid = {
+    shown:false,
+    description:"Druids belong to ancient orders that call on the forces of nature. Harnessing the magic of animals, plants, and the four elements, Druids heal, transform into animals, and wield elemental destruction."
+}
+const fighter = {
+    shown:false,
+    description:"Fighters rule many battlefields. Questing knights, royal champions, elite soldiers, and hardened mercenaries—as Fighters, they all share an unparalleled prowess with weapons and armor. And they are well acquainted with death, both meting it out and defying it."
+}
+const monk = {
+    shown:false,
+    description:"Monks use rigorous combat training and mental discipline to align themselves with the multiverse and focus their internal reservoirs of power. Different Monks conceptualize this power in various ways: as breath, energy, life force, essence, or self, for example. Whether channeled as a striking display of martial prowess or as a subtler manifestation of defense and speed, this power infuses all that a Monk does."
+}
+const paladin = {
+    shown:false,
+    description:"Paladins are united by their oaths to stand against the forces of annihilation and corruption. Whether sworn before a god’s altar, in a sacred glade before nature spirits, or in a moment of desperation and grief with the dead as the only witnesses, a Paladin’s oath is a powerful bond. It is a source of power that turns a devout warrior into a blessed champion."
+}
+const ranger = {
+    shown:false,
+    description:"Far from bustling cities, amid the trees of trackless forests and across wide plains, Rangers keep their unending watch in the wilderness. Rangers learn to track their quarry as a predator does, moving stealthily through the wilds and hiding themselves in brush and rubble."
+}
+const rogue = {
+    shown:false,
+    description:"Rogues rely on cunning, stealth, and their foes’ vulnerabilities to get the upper hand in any situation. They have a knack for finding the solution to just about any problem. A few even learn magical tricks to supplement their other abilities. Many Rogues focus on stealth and deception, while others refine skills that help them in a dungeon environment, such as climbing, finding and disarming traps, and opening locks."
+}
+const sorceror = {
+    shown:false,
+    description:"Sorcerers wield innate magic that is stamped into their being. Some Sorcerers can’t name the origin of their power, while others trace it to strange events in their personal or family history. The blessing of a dragon or a dryad at a baby’s birth or the strike of lightning from a clear sky might spark a Sorcerer’s gift."
+}
+const warlock = {
+    shown:false,
+    description:"Warlocks quest for knowledge that lies hidden in the fabric of the multiverse. They often begin their search for magical power by delving into tomes of forbidden lore, dabbling in invocations meant to attract the power of extraplanar beings, or seeking places of power where the influence of these beings can be felt. In no time, each Warlock is drawn into a binding pact with a powerful patron."
+}
+const wizard = {
+    shown:false,
+    description:"Wizards are defined by their exhaustive study of magic’s inner workings. They cast spells of explosive fire, arcing lightning, subtle deception, and spectacular transformations. Their magic conjures monsters from other planes of existence, glimpses the future, or forms protective barriers. Their mightiest spells change one substance into another, call meteors from the sky, or open portals to other worlds."
+} //I DON'T WANT TO DO THIS LET ME OUT
+
+if (barbarian = true) {
+    barbarian.shown = true;
+} else {
+    barbarian.shown = false;
+}
+if (bard = true) {
+    bard.shown = true;
+} else {
+    bard.shown = false;
+}
+if (cleric = true) {
+    cleric.shown = true;
+} else {
+    cleric.shown = false;
+}
+if (druid = true) {
+    druid.shown = true;
+} else {
+    druid.shown = false;
+}
+if (fighter = true) {
+    fighter.shown = true;
+} else {
+    fighter.shown = false;
+}
+if (monk = true) {
+    monk.shown = true;
+} else {
+    monk.shown = false;
+}
+if (paladin = true) {
+    paladin.shown = true;
+} else {
+    paladin.shown = false;
+}
+if (ranger = true) {
+    ranger.shown = true;
+} else {
+    ranger.shown = false;
+}
+if (rogue = true) {
+    rogue.shown = true;
+} else {
+    rogue.shown = false;
+}
+if (sorceror = true) {
+    sorceror.shown = true;
+} else {
+    sorceror.shown = false;
+}
+if (warlock = true) {
+    warlock.shown = true;
+} else {
+    warlock.shown = false;
+}
+if (wizard = true) {
+    wizard.shown = true;
+} else {
+    wizard.shown = false;
+}
