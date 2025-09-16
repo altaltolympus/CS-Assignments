8eec6c59a2c50bb2cb8213a058fc689a0d1c5cee
altaltolympus <luke.ofoghlu@colaistechilliain.com>
2025-09-16 10:27:45 +0100

    javba scripe

---
+++ js learning/script.js
@@ -0,0 +1,16 @@
+let innocuous = document.getElementById("innocuous");
+let innocuousText = innocuous.innerText;
+console.log("oo wee oo");
+console.log(innocuous);
+console.debug(innocuousText);
+
+// i yearn for blood
+
+function crazyRight() {
+    innocuous.innerText = "oo wee oo";
+    innocuous.style.fontFamily = "monospace";
+    window.alert("you have 37 diseases");
+    document.body.style.backgroundColor = "#161d16";
+}
+
+let array = [];
