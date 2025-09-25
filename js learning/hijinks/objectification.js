9b27053383855c4f0aafb938f171cee3cb3f1dcd
altaltolympus <luke.ofoghlu@colaistechilliain.com>
2025-09-25 10:28:51 +0100

    vimeo gams

---
+++ js learning/hijinks/objectification.js
@@ -0,0 +1,40 @@
+let character = {
+    maxHealth: 100,
+    currentHealth: 100,
+    maxMana: 100,
+    currentMana: 100,
+    baseDefense: 0,
+    baseAttack: 0,
+    baseSpellcasting: 0,
+    totalDefense: 0,
+    totalAttack: 0,
+    totalSpellcasting: 0,
+    defenseItem: "",
+    attackItem: "",
+    spellcastingItem: "",
+};
+let defenseItem1 = {
+    name: "Leather Vest",
+    defense: 10,
+};
+let defenseItem2 = {
+    name: "Padded Chainmail",
+    defense: 25,
+};
+let defenseItem3 = {
+    name: "Full Plate Armour",
+    defense: 50,
+};
+let attackItem1 = {
+    name: "Rusty Dagger",
+    attack: 10,
+};
+let attackItem2 = {
+    name: "Arming Sword",
+    attack: 25,
+};
+let attackItem3 = {
+    name: "Bastard Sword",
+    attack: 50,
+};
+let SpellcastingItem = {};
