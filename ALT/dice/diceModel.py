b9e0c04010b75dc134f4850c3d0f4f3026555894
altaltolympus <luke.ofoghlu@colaistechilliain.com>
2025-11-24 11:52:05 +0000

    horrible horrible functions

--- ALT/dice/diceModel.py
+++ ALT/dice/diceModel.py
@@ -1,4 +1,6 @@
 from random import *
+from matplotlib.pyplot import *
+diceNotation = ""
 
 def dieRoll(amount, size):
     diceResult = []
@@ -6,27 +8,42 @@ def dieRoll(amount, size):
         diceResult.append(randint(1, size))
     return diceResult
 
-def modelRun(amount, size, iterations):
+def diceRollLoop(amount, size, iterations):
     manyResults = []
     for run in range(iterations):
         output = dieRoll(amount, size)
         manyResults.append([output, sum(output)])
+    diceNotation = f"{amount}d{size} rolled {iterations} time(s)"
     return manyResults
 
 def probabilityCheck(amount, size, iterations):
-    inputMatrix = modelRun(amount, size, iterations)
+    inputMatrix = diceRollLoop(amount, size, iterations)
     allSums = []
     for item in inputMatrix:
         allSums.append(item[1])
     allSums.sort()
-    print(allSums)
+    # print(allSums)
     return allSums
+
+def consolidate(list):
+    counter = 0
+    frequencies = []
+    while counter < list[-1]:
+        valueFrequency = [counter, 0]
+        for item in list:
+            if item == counter: valueFrequency[1] += 1
+        frequencies.append(valueFrequency)
+        counter += 1
+    return frequencies
+
+def runEverything(amount, size, iterations):
+    return consolidate(probabilityCheck(diceRollLoop(amount, size, iterations)))
 
 
 def test():
+    print(consolidate(probabilityCheck(3, 10, 1000))) # action surge eldritch blast
     # print(dieRoll(8, 6)) # 3rd-level fireball
-    # print(dieRoll(1, 20)) # skill check
-    # print(modelRun(2, 6, 4)) # 3rd-level scorching ray
-    print(probabilityCheck(3, 10, 2)) # action surge eldritch blast
+    # print(diceRollLoop(2, 6, 4)) # 3rd-level scorching ray   
+    # print(probabilityCheck(2, 4, 100)) #greatclub attack
 
 test()
