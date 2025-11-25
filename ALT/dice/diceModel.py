1e6790678df68ae3453a379ffad7cecbadc7f919
altaltolympus <luke.ofoghlu@colaistechilliain.com>
2025-11-25 10:01:28 +0000

    HASTE!!

--- ALT/dice/diceModel.py
+++ ALT/dice/diceModel.py
@@ -14,6 +14,7 @@ def diceRollLoop(amount, size, iterations):
         output = dieRoll(amount, size)
         manyResults.append([output, sum(output)])
     diceNotation = f"{amount}d{size} rolled {iterations} time(s)"
+    # print(manyResults)
     return manyResults
 
 def probabilityCheck(amount, size, iterations):
@@ -34,16 +35,31 @@ def consolidate(list):
             if item == counter: valueFrequency[1] += 1
         frequencies.append(valueFrequency)
         counter += 1
+    print(frequencies)
     return frequencies
 
+def coordinateify(matrix):
+    x = []
+    y = []
+    for item in matrix:
+        x.append(item[0])
+        y.append(item[1])
+    return [x, y]
+
 def runEverything(amount, size, iterations):
-    return consolidate(probabilityCheck(diceRollLoop(amount, size, iterations)))
+    return coordinateify(consolidate(probabilityCheck(amount, size, iterations)))
 
 
 def test():
-    print(consolidate(probabilityCheck(3, 10, 1000))) # action surge eldritch blast
+    # print(consolidate(probabilityCheck(3, 10, 1000))) # action surge eldritch blast
     # print(dieRoll(8, 6)) # 3rd-level fireball
     # print(diceRollLoop(2, 6, 4)) # 3rd-level scorching ray   
-    # print(probabilityCheck(2, 4, 100)) #greatclub attack
+    # print(probabilityCheck(2, 4, 100)) # greatclub attack
+    # print(f"\n{runEverything(15, 8, 1000000)}") # finger of death
+    output = runEverything(2, 6, 1000000)
+    print(output)
+    # scatter(output[0], output[1])
+    plot(output[1], marker=".")
+    show()
 
 test()
