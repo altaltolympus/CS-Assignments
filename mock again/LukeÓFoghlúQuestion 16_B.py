e0b3edfe1f9cba939a1af0e6d4badaff66c327a8
altaltolympus <luke.ofoghlu@colaistechilliain.com>
2026-04-27 11:49:09 +0100

    nobody has ever been better than me

--- mock again/LukeÓFoghlúQuestion 16_B.py
+++ mock again/LukeÓFoghlúQuestion 16_B.py
@@ -5,7 +5,7 @@
 from random import *
 
 def random_item(list):
-    return list[randint(0, len(list))]
+    return list[randint(0, len(list)-1)]
 
 
 one_syllable = ["dusk", "rain", "cloud", "stars", "mist", "wind", "flame", "stone", "sky"]
@@ -17,25 +17,17 @@ five_syllable = ["a distant thunder", "beneath silent trees","whispers in the gr
 phrases = [one_syllable, two_syllable, three_syllable, four_syllable, five_syllable]
 
 def haiku_line(length):
-    start_syllables = randint(0,4)
-    print(f"start_syllables: {start_syllables}")
-    end_syllables = length - start_syllables
-    
-    print(f"end_syllables: {end_syllables}")
-    
-    start_phrases = phrases[start_syllables]
-    print(f"start_phrases: {start_phrases}")
-    line = random_item(start_phrases)
-    print(f"line: {line}")
-    if end_syllables > -1:
-        end_phrases = phrases[end_syllables]
-        print(f"end_phrases: {end_phrases}")
-        line += f" {random_item(end_phrases)}"
-        print(f"completed line: {line}")
+    line = ""
+    length_distribution = []
+    while sum(length_distribution) < length:
+        length_distribution.append(randint(1, min(length - sum(length_distribution), 5)))
+    for phrase_length in length_distribution:
+        phrase_length -= 1
+        line += f" {random_item(phrases[phrase_length])}"
     return line
         
 def generate_haiku():
-    print(f"{haiku_line(5)}\n{haiku_line(7)}\n{haiku_line(5)}")
+    print(f"\n{haiku_line(5)}\n{haiku_line(7)}\n{haiku_line(5)}")
     repeat = input('another? (y/n)\n > ')
     if repeat == "y":
         generate_haiku()
